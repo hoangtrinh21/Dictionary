c5e68f4c32223b37a29edfb61fc3a068e565d60c
trnhhoang <trinhhhoang2001bn@gmail.com>
2020-09-20 21:44:00 +0700

    class Word/ Trinh Hoang

---
+++ Word.java
@@ -0,0 +1,24 @@
+public class Word {
+    private String word_target;
+    private String word_explain;
+    public String getWordTarget() {
+        return this.word_target;
+    }
+    public String getWordExplain() {
+        return this.word_explain;
+    }
+    public void setWordTarget(String x) {
+        this.word_target = x;
+    }
+    public void steWordExplain(String x) {
+        this.word_explain = x;
+    }
+    public Word() {
+        this.word_target = " ";
+        this.word_explain = " ";
+    }
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+}
