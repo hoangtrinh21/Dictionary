271466c0b2ca897546ce4b35574356e342a196c3
lemanhlinh88 <63782267+lemanhlinh88@users.noreply.github.com>
2020-09-28 00:19:20 +0700

    them tu file

--- DictionaryManagement.java
+++ DictionaryManagement.java
@@ -3,27 +3,37 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class DictionaryManagement {
-
     void insertFromCommandline(Dictionary list){
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
-            scanner.nextLine();
+        scanner.nextLine();
         for(int i=0;i<n;i++){
             Word x = new Word();
-            String a = scanner.nextLine();
+            String a =scanner.nextLine();
             String b = scanner.nextLine();
             x.setWordTarget(a);
             x.setWordExplain(b);
             list.listWord.add(x);
         }
     }
-//    public void addFile() throws IOException {
-//        Scanner scanner=new Scanner(Paths.get("dictionaries.txt"),"UTF-8");
-//        while(scanner.hasNext()){
-//            String a=scanner.nextLine();
-//            Word x=new Word();
-//            x.setWordTarget();
-//        }
-//        scanner.close();
-//    }
+    public void addFile(Dictionary list) throws IOException {
+        Scanner scanner=new Scanner(Paths.get("C:\\Users\\leman\\IdeaProjects\\lemanhlinh\\src\\dictionaries.txt"),"UTF-8");while(scanner.hasNext()){
+            while(scanner.hasNextLine()){
+                Word x=new Word();
+                String a=scanner.next();
+                x.setWordTarget(a);
+                String b=scanner.nextLine();
+                for(int i=0;i<10;i++){
+                    if(!Character.toString(b.charAt(i)).equals(" ")){
+                        String c=b.substring(i);
+                        x.setWordExplain(c);
+                        break;
+                    }
+                }
+                list.listWord.add(x);
+            }
+        }
+        scanner.close();
+    }
 }
+// hel lo    xin chao
