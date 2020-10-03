6171c2a29a2f5d5d76774fb0f811cc79d788fc25
lemanhlinh88 <63782267+lemanhlinh88@users.noreply.github.com>
2020-10-03 11:04:49 +0700

    Add files via upload
    them search

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -1,28 +1,42 @@
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.*;
+import java.util.Scanner;
 
 public class DictionaryCommandline {
     DictionaryManagement in = new DictionaryManagement();
-    public static void showAllWords(Dictionary a) {
+    public void showAllWords(Dictionary a) {
         System.out.println("No | English           | Vietnamese");
         for (int i = 0;i < a.listWord.size(); i++){
             System.out.printf("%-3s| %-20s|%s%n", i + 1, a.listWord.get(i).getWordTarget(), a.listWord.get(i).getWordExplain());
         }
     }
-    
     public void dictionaryAdvanced(Dictionary list) throws IOException {
-        in.insertFromFile(list);
+        DictionaryManagement a=new DictionaryManagement();
+        a.insertFromFile(list);
         showAllWords(list);
         Scanner sc = new Scanner(System.in);
         String tar = sc.next();
-        in.dictionaryLookup(list, tar);
+        a.dictionaryLookup(list, tar);
     }
-
     public void dictionaryBasic(Dictionary list) throws FileNotFoundException, UnsupportedEncodingException {
         in.insertFromCommandline(list);
         showAllWords(list);
+    }
+    public Dictionary dictionarySearcher(Dictionary list){
+        Scanner scanner=new Scanner(System.in);
+        String s=scanner.nextLine();
+        Dictionary a=new Dictionary();
+        int n=s.length();
+        for(Word i: list.listWord){
+            if(i.getWordTarget().length()>=n){
+                String string=i.getWordTarget().substring(0,n);
+                if(string.equals(s)){
+                    Word word=new Word(i.getWordTarget(),i.getWordExplain());
+                    a.listWord.add(word);
+                }
+            }
+        }
+        return a;
     }
 }
