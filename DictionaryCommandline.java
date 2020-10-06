58dbe6f62569ab9bf5cbde920fef09bc4a26237a
longk64cb <63768890+longk64cb@users.noreply.github.com>
2020-10-06 23:18:01 +0700

    Sửa lại hàm in từ điển

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -9,7 +9,7 @@ public class DictionaryCommandline {
         System.out.println("------------------------------------------------");
         System.out.println("No | English           | Vietnamese");
         for (int i = 0;i < a.listWord.size(); i++){
-            System.out.printf("%-3s| %-20s|%s%n", i + 1, a.listWord.get(i).getWordTarget(), a.listWord.get(i).getWordExplain());
+            System.out.printf("%-3s| %-20s| %s%n", i + 1, a.listWord.get(i).getWordTarget(), a.listWord.get(i).getWordExplain());
         }
     }
     
