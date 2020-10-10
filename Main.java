3adea4e9a0cc0545c1b23bf4498972c2f3fc3a25
lemanhlinh88 <63782267+lemanhlinh88@users.noreply.github.com>
2020-10-10 23:07:21 +0700

    Add files via upload
    đồ họa sơ khai

--- Main.java
+++ Main.java
@@ -1,42 +1,25 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Scanner;
+package sample;
 
-public class Main {
-    public static void main(String[] args) throws IOException, FileNotFoundException, UnsupportedEncodingException {
-        Scanner scan = new Scanner(System.in);
-        Dictionary lWord = new Dictionary();
-        DictionaryCommandline show = new DictionaryCommandline();
-        DictionaryManagement in = new DictionaryManagement();
-        in.insertFromFile(lWord);
-        //show.showAllWords(lWord);
-        while (true) {
-            System.out.println("------------------------------------------------");
-            System.out.println("Bạn muốn làm gì?" + '\n' +
-                                "1: Tra từ" + '\n' + 
-                                "2: Sửa, thêm , xóa" + '\n' +
-                                "3: Hiện toàn bộ thư viện" + '\n' +
-                                "4: Thoát");
-            System.out.println("------------------------------------------------");
-            int x = scan.nextInt();
-            scan.nextLine();
-            if (x == 1) {
-                System.out.println("-------------------");
-                System.out.println("Từ bạn muốn tìm là?");
-                String search = scan.nextLine().toLowerCase();
-                
-                show.dictionarySearcher(lWord, search);
-            }
-            if (x == 2) {
-                in.suaXoaThem(lWord);
-            }
-            if (x == 3) {
-                show.showAllWords(lWord);
-            }
-            if (x == 4) {
-                return;
-            }
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        try {
+            Parent root=FXMLLoader.load(getClass().getResource("sample.fxml"));
+            Scene scene=new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
+    }
+    public static void main(String[] args) {
+        launch(args);
     }
 }
